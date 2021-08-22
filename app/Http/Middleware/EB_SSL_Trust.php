a6724faa40f02718bb18e180359e5cbda2aaa602
t49s94 <86166259+t49s94@users.noreply.github.com>
2021-08-22 13:08:16 -0400

    Added foo to the bar

--- app/Http/Middleware/EB_SSL_Trust.php
+++ app/Http/Middleware/EB_SSL_Trust.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 
+use Illuminate\Support\Facades\App;
+
 class EB_SSL_Trust
 {
     /**
@@ -15,8 +17,9 @@ class EB_SSL_Trust
      */
     public function handle($request, Closure $next)
     {
-      if(! $request->secure())
-        return redirect()->secure($request->path());
+      if (!$request->secure() && App::environment() === 'production') {
+        return redirect()->secure($request->getRequestUri());
+      }
 
       return $next($request);
     }

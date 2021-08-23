0dcc4782ace767ca0cbeaf6a0fd0334005cc251b
t49s94 <86166259+t49s94@users.noreply.github.com>
2021-08-23 12:38:17 -0400

    Added foo to the bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -88,6 +88,28 @@ https://stackoverflow.com/questions/50262517/bootstrap-4-row-fill-remaining-heig
                                   {{ __('Meet the team!') }}
                               </a>
 
+                              <li class="dropdown-item nav-item dropdown pr-3">
+                                  <a id="navbarDropdown" class="nav-link dropdown-toggle text-primary" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                      <strong>About us</strong> <span class="caret"></span>
+                                  </a>
+
+                                  <div class="dropdown-menu dropdown-menu-right py-0" aria-labelledby="navbarDropdown" style="">
+
+                                      <a class="dropdown-item text-primary" href="{{ url('/contact-us/create/') }}">
+                                          {{ __('Contact us') }}
+                                      </a>
+
+                                      <a class="dropdown-item text-primary" href="{{ url('/services/index/') }}">
+                                          {{ __('Services') }}
+                                      </a>
+
+                                      <a class="dropdown-item text-primary" href="{{ url('/employees/team/') }}">
+                                          {{ __('Meet the team!') }}
+                                      </a>
+
+                                  </div>
+                              </li>
+
                           </div>
                       </li>
 
@@ -132,27 +154,7 @@ https://stackoverflow.com/questions/50262517/bootstrap-4-row-fill-remaining-heig
                                       {{ __('Testimonials') }}
                                   </a>
 
-                                  <li class="nav-item dropdown pr-3">
-                                      <a id="navbarDropdown" class="nav-link dropdown-toggle text-primary" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                          <strong>About us</strong> <span class="caret"></span>
-                                      </a>
 
-                                      <div class="dropdown-menu dropdown-menu-right py-0" aria-labelledby="navbarDropdown" style="">
-
-                                          <a class="dropdown-item text-primary" href="{{ url('/contact-us/create/') }}">
-                                              {{ __('Contact us') }}
-                                          </a>
-
-                                          <a class="dropdown-item text-primary" href="{{ url('/services/index/') }}">
-                                              {{ __('Services') }}
-                                          </a>
-
-                                          <a class="dropdown-item text-primary" href="{{ url('/employees/team/') }}">
-                                              {{ __('Meet the team!') }}
-                                          </a>
-
-                                      </div>
-                                  </li>
 
 
 

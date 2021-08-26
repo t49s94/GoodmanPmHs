f1dd03acaf16cff0f16d9fae31ca717c34c5fea2
t49s94 <86166259+t49s94@users.noreply.github.com>
2021-08-26 12:12:11 -0400

    Added foo to the bar

--- resources/views/testimonials/index.blade.php
+++ resources/views/testimonials/index.blade.php
@@ -73,6 +73,12 @@ https://examples.bootstrap-table.com/#welcome.html
 </div>
 <table
   id="table"
+  data-show-pagination-switch="true"
+  data-pagination="true"
+  data-id-field="id"
+  data-page-list="[10, 25, 50, 100, all]"
+  data-show-footer="true"
+  data-side-pagination="server"
   >
 </table>
 <!--
@@ -129,8 +135,8 @@ $(window).on('load', function() {
   var selections = []
 
   var data = {
-  "total": 6,
-  "totalNotFiltered": 6,
+  "total": 21,
+  "totalNotFiltered": 21,
   "rows":[
     {
       'id': 0,
@@ -161,6 +167,81 @@ $(window).on('load', function() {
       'id': 5,
       'name': 'Item 5',
       'price': '$5'
+    },
+    {
+      "id": 6,
+      "name": "Item 6",
+      "price": "$6"
+    },
+    {
+      "id": 7,
+      "name": "Item 7",
+      "price": "$7"
+    },
+    {
+      "id": 8,
+      "name": "Item 8",
+      "price": "$8"
+    },
+    {
+      "id": 9,
+      "name": "Item 9",
+      "price": "$9"
+    },
+    {
+      "id": 10,
+      "name": "Item 10",
+      "price": "$10"
+    },
+    {
+      "id": 11,
+      "name": "Item 11",
+      "price": "$11"
+    },
+    {
+      "id": 12,
+      "name": "Item 12",
+      "price": "$12"
+    },
+    {
+      "id": 13,
+      "name": "Item 13",
+      "price": "$13"
+    },
+    {
+      "id": 14,
+      "name": "Item 14",
+      "price": "$14"
+    },
+    {
+      "id": 15,
+      "name": "Item 15",
+      "price": "$15"
+    },
+    {
+      "id": 16,
+      "name": "Item 16",
+      "price": "$16"
+    },
+    {
+      "id": 17,
+      "name": "Item 17",
+      "price": "$17"
+    },
+    {
+      "id": 18,
+      "name": "Item 18",
+      "price": "$18"
+    },
+    {
+      "id": 19,
+      "name": "Item 19",
+      "price": "$19"
+    },
+    {
+      "id": 20,
+      "name": "Item 20",
+      "price": "$20"
     }
   ]
   }
@@ -271,13 +352,6 @@ $(window).on('load', function() {
           sortable: true,
           align: 'center',
           footerFormatter: totalPriceFormatter
-        }, {
-          field: 'operate',
-          title: 'Item Operate',
-          align: 'center',
-          clickToSelect: false,
-          events: window.operateEvents,
-          formatter: operateFormatter
         }]
       ],
       data: data

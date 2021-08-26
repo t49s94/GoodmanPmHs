0ea47246f634b785109f8dba8c3211cd6793f050
t49s94 <86166259+t49s94@users.noreply.github.com>
2021-08-26 13:35:42 -0400

    Added foo to the bar

--- resources/views/testimonials/index.blade.php
+++ resources/views/testimonials/index.blade.php
@@ -284,8 +284,7 @@ $(window).on('load', function() {
           align: 'center',
           footerFormatter: totalPriceFormatter
         }]
-      ],
-      data: data
+      ]
     })
     $table.on('check.bs.table uncheck.bs.table ' +
       'check-all.bs.table uncheck-all.bs.table',

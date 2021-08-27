566bc9fbf633db3e8cd7cd5e302d41faec92d535
t49s94 <86166259+t49s94@users.noreply.github.com>
2021-08-27 16:11:30 -0400

    Added foo to the bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -187,6 +187,7 @@
 
 @push('head')
 
+<script src="{{asset('moment.js')}}" type="text/javascript" ></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
 
 <script>

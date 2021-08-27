a772633ce1a89ede6de4ca3b3881be42494b2071
t49s94 <86166259+t49s94@users.noreply.github.com>
2021-08-27 13:06:52 -0400

    Added foo to the bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -194,57 +194,59 @@
 <script src="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table-locale-all.min.js" defer></script>
 <script src="https://unpkg.com/bootstrap-table@1.18.3/dist/extensions/export/bootstrap-table-export.min.js" defer></script>
 
-<script>
+<script defer>
 
 (function($){
+
+  var $table = $('#table')
+
+  $(function() {
+    var data = [
+      {
+        'id': 0,
+        'name': 'Item 0',
+        'price': '$0'
+      },
+      {
+        'id': 1,
+        'name': 'Item 1',
+        'price': '$1'
+      },
+      {
+        'id': 2,
+        'name': 'Item 2',
+        'price': '$2'
+      },
+      {
+        'id': 3,
+        'name': 'Item 3',
+        'price': '$3'
+      },
+      {
+        'id': 4,
+        'name': 'Item 4',
+        'price': '$4'
+      },
+      {
+        'id': 5,
+        'name': 'Item 5',
+        'price': '$5'
+      }
+    ]
+    console.log("entro")
+    $table.bootstrapTable({data: data})
+  })
 
 //funciona
   $( window ).on('unload', function( event ) {
 
 
-    var $table = $('#table')
 
-    $(function() {
-      var data = [
-        {
-          'id': 0,
-          'name': 'Item 0',
-          'price': '$0'
-        },
-        {
-          'id': 1,
-          'name': 'Item 1',
-          'price': '$1'
-        },
-        {
-          'id': 2,
-          'name': 'Item 2',
-          'price': '$2'
-        },
-        {
-          'id': 3,
-          'name': 'Item 3',
-          'price': '$3'
-        },
-        {
-          'id': 4,
-          'name': 'Item 4',
-          'price': '$4'
-        },
-        {
-          'id': 5,
-          'name': 'Item 5',
-          'price': '$5'
-        }
-      ]
-      console.log("entro")
-      $table.bootstrapTable({data: data})
-    })
 
 
 
     alert("miau")
-});
+})
 
 })(jQuery)
 
